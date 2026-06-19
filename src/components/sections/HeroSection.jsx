3177958a117f2ea5c25fa6c4eddd72839c2b21a7
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-06-19 14:00:29 -0700

    Enable buttons and format files

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef } from 'react';
-import Button from '../common/Button';
-import heroSvgRaw from '../../assets/images/two-people-pile-of-money.svg?raw';
-import { initHeroAnimations, prepareHeroSvg } from '../../utils/heroAnimations';
+import { useEffect, useRef } from "react";
+import Button from "../common/Button";
+import heroSvgRaw from "../../assets/images/two-people-pile-of-money.svg?raw";
+import { initHeroAnimations, prepareHeroSvg } from "../../utils/heroAnimations";
 
 const heroSvgMarkup = prepareHeroSvg(heroSvgRaw);
 
@@ -21,7 +21,12 @@ function HeroSection() {
   }, []);
 
   return (
-    <section id="hero" className="hero" ref={heroRef} aria-labelledby="hero-heading">
+    <section
+      id="hero"
+      className="hero"
+      ref={heroRef}
+      aria-labelledby="hero-heading"
+    >
       <div className="hero__background" aria-hidden="true">
         <div className="hero__shape hero__shape--1" />
         <div className="hero__shape hero__shape--2" />
@@ -32,12 +37,13 @@ function HeroSection() {
         <div className="hero__content" ref={contentRef}>
           <h1 id="hero-heading">Take control of your financial future</h1>
           <p className="hero__subheadline">
-            Aim makes it simple to invest, save, and plan for the goals that matter most to
-            you. Whether you&apos;re saving for a home, retirement, or just building wealth,
-            we&apos;ve got the tools to get you there.
+            Aim makes it simple to invest, save, and plan for the goals that
+            matter most to you. Whether you&apos;re saving for a home,
+            retirement, or just building wealth, we&apos;ve got the tools to get
+            you there.
           </p>
           <div className="hero__actions">
-            <Button variant="primary" size="large" type="button" disabled>
+            <Button variant="primary" size="large" type="button">
               Get Started Free
             </Button>
             <Button variant="text" href="#how-it-works">
